e5f2022b4570362d2c6d8227c36d673817479753
Savage <murithijeff276@gmail.com>
2025-09-18 15:22:08 +0300

    Add files via upload
    Added the assignmens

---
+++ Units.c
@@ -0,0 +1,22 @@
+//simple c program (BACHELOF COMPUTER SCIENCE;SEME 1 UNITS)
+/*
+Name: JEFF MURITHI 
+Reg no.:CT101/G/26553/25
+Description: Bachelor of computer science units
+*/
+
+#include <stdio.h>
+int main(){
+
+    printf("UNITS FOR BACHELOR OF COMPUTER SCIENCE \n");
+    printf("1. Imtroduction to programming and problem solving \n");
+    printf("2. Fundamentals of ICT \n");
+    printf("3. Discrete Mathematics \n");
+    printf("4. Mathematics for Science \n");
+    printf("5. Physics \n");
+    printf("6. Introduction to computer Architecture \n");
+    printf("7. Communication Skills \n");
+    printf("8. Analogue Electronics \n");
+    
+    return 0; 
+}    
---
+++ medical.c
@@ -0,0 +1,45 @@
+//Simple c program (APPLICATION FOR MEDICAL CHECK UP )
+/*
+Name:JEFF MURITHI 
+Reg no:CT101/G/26553/25
+Description:APPLICATION FOR MEDICAL CHECK UP
+*/
+
+#include <stdio.h>
+int main(){
+
+printf("APPLICATION FOR MEDICAL CHECK UP \n");
+
+   int weight;
+    printf("enter your weight (in kilogrammes )\n");
+    scanf("%d",&weight);
+    printf("your weight is %d \n",weight);
+    
+   int age;
+    printf("enter your age(in years) \n");
+    scanf("%d",&age);
+    printf("your age is %d \n",age);
+    
+   int height;
+    printf("enter your height (in centimetres)\n");
+    scanf("%d",&height);
+    printf("your height is \n",height);
+    
+   int balance;
+    printf("enter your bank balance(in numbers) \n");
+    scanf("%d",&balance);
+    printf("your bank balance \n",balance);
+    
+   int number;
+    printf("enter your phone number(in numbers)\n");
+    scanf("%d",&number);
+    printf("your phone number is \n",number);
+    
+   int name;
+    printf("enter your name(in letters) \n");
+    scanf("%s",&name);
+    printf("your name is \n",name);
+   
+   return 0;
+}    
+    
