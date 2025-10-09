73de98609d248f892a70a4a893f2ec7309755329
JEFF <murithijeff276@gmail.com>
2025-10-09 12:15:24 +0300

    Add files via upload
    For loop to print all numbers from 100 to 50 in descending order.

---
+++ For_loop.c
@@ -0,0 +1,17 @@
+/*
+Name:JEFF MURITHI.
+Reg.No:CT101/G/26553/25.
+Date:9th October 2025
+Description:
+*/
+
+#include <stdio.h>
+
+  int main(){
+  	int i=0;
+  	for(i=100;i>=50;i--){
+		  printf("%d\n",i);
+	  }
+  	  
+	  return 0;
+  }
