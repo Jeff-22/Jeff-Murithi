9a20d6ba23106b24817b0475a159de2b954baa7c
JEFF <murithijeff276@gmail.com>
2025-10-30 10:53:05 +0300

    Add files via upload
    Arrays

---
+++ 1D_Array.c
@@ -0,0 +1,28 @@
+/*
+Name:
+Reg.No:
+Date:
+Description:
+*/
+
+#include <stdio.h>
+
+int main(){
+	int revenue[7],total=0;
+	float average;
+	int i;
+	
+	printf("Enter revenue for 7 days: \n");
+	for(i=0;i<7;i++){
+		printf("Day %d:",i+1);
+		scanf("%d",&revenue[i]);
+		total+=revenue[i];
+	}
+	
+	average=total/7.0;
+	
+	printf("Total weekly revenue:%d \n",total);
+	printf("Average daily revenue:%.2f \n",average);
+	
+	return 0;
+}
